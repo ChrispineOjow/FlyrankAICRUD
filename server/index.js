2b1754585c62344952d2f35b8fa90e11159e5ddf
Chrispine Ojow <WameyoChrispine@gmail.com>
2026-07-23 01:45:04 +0300

    Added in memory data and also added adding and getting tasks

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.json());
 
+//In Memory "database" pre-filled with 3 example tasks
+
+let tasks = [
+    {id: 1, title: "Intern with FlyRank AI", done: true},
+    {id: 2, title: "Build a task API", done: false},
+    {id: 3, title: "Get a job", done: false}
+];
+
+//Root endpoint
 app.get("/", (req, res)=> {
     res.json({
 
@@ -14,10 +23,69 @@ app.get("/", (req, res)=> {
     })
 })
 
+
 app.get("/health", (req, res)=> {
     res.json({
         "status": "OK"
     })
+})
+
+//return all tasks
+app.get("/tasks", (req, res)=> {
+    try{
+        res.json(tasks)
+    }catch(error){
+        res.status(error.message)
+    }
+})
+
+//return a single task by id
+app.get("/tasks/:id", (req, res)=>{
+    try{
+
+        const taskId = parseInt(req.params.id);
+        const task = tasks.find(t => t.id === taskId);
+
+        if(!task){
+            res.status(404).json ({error: `Task ${taskId} not found`, message: error.message});
+        }
+
+        res.status(200).json(task);
+
+    }catch(error){
+
+        res.status(500).json({error: "Internal Server Error"});
+
+    }
+})
+
+//Add a new task
+app.post("/tasks", (req, res)=> {
+    try{
+
+        const {title} = req.body;
+
+        if(!title || title.trim() === ""){
+            return res.status(400).json({error: "Title is required"});
+        }
+
+        const nextId = tasks.length > 0 ? Math.max( ...tasks.map(t=> t.id))+1 : 1;
+
+        const newTask = {
+            id: nextId,
+            title: title.trim(),
+            done: false
+        }
+
+        tasks.push(newTask);
+
+        res.status(201).json(newTask);
+
+    }catch(error){
+
+        res.status(500).json({error: "Internal Server Error"});
+
+    }
 })
 
 
