b2d82734697aefe5b0463eda086ab3f5268a56fe
Chrispine Ojow <WameyoChrispine@gmail.com>
2026-07-23 00:52:27 +0300

    root and health endpoints

--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ import express from "express";
 const PORT = 5000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res)=> {
-    try{
-    res.send("Hello Server");
-    }catch(error){
-        console.log("Error ", error.message);
-    }
+    res.json({
+
+        "name": "Task API",
+        "version":"1.0",
+        "endpoints":["/tasks"]
+    })
 })
+
+app.get("/health", (req, res)=> {
+    res.json({
+        "status": "OK"
+    })
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
